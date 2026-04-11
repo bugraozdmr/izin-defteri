bc564af8af7b3a4ff9ef477222d28d0793728de0
bugra <bugra@linuxcum.com>
2026-04-12 00:27:48 +0300

    fix

--- src/features/product/actions/product.action.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { fetchAllProducts } from "../service/product.service";
-
-export const getProductsAction = async () => {
-    // Burada gerekirse kullanıcının yetkisi var mı (auth) kontrolü de yapılabilir.
-    return await fetchAllProducts();
-};
--- src/features/product/repository/product.repository.ts
+++
@@ -1,7 +0,0 @@
-import { db } from "@/lib/db";
-
-export const getAllProducts = async () => {
-  return await db.product.findMany({
-    orderBy: { createdAt: "desc" },
-  });
-};
