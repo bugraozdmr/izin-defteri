7d94c727225b0edaf5bca23858b5fd9df44d1cbd
bugra <bugra@linuxcum.com>
2026-04-12 00:26:25 +0300

    fix

--- src/api/products/route.ts
+++
@@ -1,9 +0,0 @@
-import { db } from '@/lib/db'
-
-export const runtime = 'nodejs'
-
-export async function GET() {
-  const products = await db.product.findMany()
-
-  return Response.json(products)
-}
